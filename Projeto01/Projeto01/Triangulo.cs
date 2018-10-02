32e93565944610ea7ee0df4187be9689613b6fba
Elton dos Santos Andrade <eltonsa75@hotmail.com>
2018-10-01 21:05:44 -0300

    Projeto Orientação Objeto
    Projeto Orientação Objeto

---
+++ Projeto01/Projeto01/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projeto01
+{
+    class Triangulo
+    {
+        public double a, b, c;
+
+        public double area()
+        {
+            double  p = (a + b + c) / 2.0;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
