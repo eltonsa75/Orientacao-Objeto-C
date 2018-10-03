51bc59eb8a9b78d3367acdb97191a3e131ba475a
Elton dos Santos Andrade <eltonsa75@hotmail.com>
2018-10-02 22:14:19 -0300

    Conceito de Lista
    Conceito de Lista

---
+++ Lista/Lista/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/* Biblioteca para Lista */
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* conteito de Lita */
+            List<Produto> lista = new List<Produto>();
+
+            lista.Add(new Produto("TV", 900.00));
+            lista.Add(new Produto("Aspirador", 400.00));
+            lista.Add(new Produto("Geladeira", 2000.00));
+           
+
+            int pos = lista.FindIndex(x => x.preco>3000);
+            Console.WriteLine("Posição encontrada  = " + pos);
+
+            for (int i=0; i<lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
