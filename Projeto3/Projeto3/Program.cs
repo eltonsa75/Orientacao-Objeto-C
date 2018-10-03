145e1c51022f1ec9d082228385e98ac318780590
Elton dos Santos Andrade <eltonsa75@hotmail.com>
2018-10-02 22:12:47 -0300

    Projeto 3
    Projeto 3

---
+++ Projeto3/Projeto3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exemplo de vetor de Produto */
+            Produto[] vet;
+            double soma, media;
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new Produto[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                String nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Produto(nome, preco);
+            }
+
+            soma = 0.0;
+            for (int i=0; i<N; i++)
+            {
+                soma = soma + vet[i].preco;
+            }
+            media = soma / N;
+
+            Console.WriteLine("PREÇO MEDIA = R$ " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
