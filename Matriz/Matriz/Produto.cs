73ddf6cac9941415bf2facd1d7d4e43e7feb7f8d
Elton dos Santos Andrade <eltonsa75@hotmail.com>
2018-10-02 22:16:08 -0300

    Conceito de Matriz
    Conceito de Matriz

---
+++ Matriz/Matriz/Produto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Matriz
+{
+    class Produto
+    {
+        /* Declarar Variaveis */
+        public string nome { get; private set; }
+        public double preco;
+        public int quantidadeEmEstoque;
+
+        /* Construtor */
+        public Produto(string nome, double preco, int quantidadeEmEstoque)
+        {
+            /* This é uma referencia ao próprio objeto */
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        /* SobreCarga aonde inicializo a quantidade com valor 0*/
+        public Produto(string nome, double preco)
+        {
+            /* This é uma referencia ao próprio objeto */
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = 0;
+        }
+
+        /* Emplementação dos Metodos */
+        public double valorTotalEmEstoque()
+        {
+            return preco * quantidadeEmEstoque;
+        }
+
+
+        public void realizarEntrada(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque + quantidade;
+        }
+
+        public void realizarSaida(int quantidade)
+        {
+            quantidadeEmEstoque = quantidadeEmEstoque - quantidade;
+        }
+        public override string ToString()
+        {
+            return nome
+                + " , R$ "
+                + preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + quantidadeEmEstoque
+                + " unidades, Total: R$: "
+                + valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Matriz/Matriz/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat;
+            int N, cont;
+
+            N = int.Parse(Console.ReadLine());
+            mat = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] linha = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(linha[j]);
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL: ");
+            for (int i = 0; i < N; i++)
+            {
+                /* Exibir linha i coluna i */
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            cont = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        cont++;
+                    }
+                }
+            }
+
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS = " + cont);
+
+            Console.ReadLine();
+        }
+    }
+}
+           
+    
+       
+
